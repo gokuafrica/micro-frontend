d2a3bf7e5864ad43dae759b2775931b766c4c144
gokuafrica <anweshmohapatra007@gmail.com>
2020-10-01 04:09:43 +0530

    Initial commit

---
+++ app2/src/root.component.js
@@ -0,0 +1,10 @@
+import React from "react";
+import Button from "@material-ui/core/Button";
+import './css/style.css';
+export default function Root(props) {
+  return (
+    <h1><center>
+      This is the micro-frontend gateway!
+      </center></h1>
+  );
+}
